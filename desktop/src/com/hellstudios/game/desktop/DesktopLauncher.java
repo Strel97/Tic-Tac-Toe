4d5ddf981ee6a0018565af95eebc82a536b8b624
strel97 <strel97@gmail.com>
2017-04-03 22:31:42 +0300

    Setting up LibGDX environment

---
+++ desktop/src/com/hellstudios/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hellstudios.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hellstudios.game.TicTacToe;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TicTacToe(), config);
+	}
+}
